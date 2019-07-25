3bb2718d218d0c0163706a5c5ba5db84445a479b
heladio <heladio@artefactoestudio.com>
2019-07-25 14:11:55 -0500

    1

---
+++ Assets/DataInserter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataInserter : MonoBehaviour
+{
+    public string inputName;
+    public string inputMail;
+
+    string CreateUserURL= "https://databasechroma.000webhostapp.com/itemsData.php";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateUser(inputName, inputMail);
+            Debug.Log("datos subidos correctamente");
+        }
+    }
+    public void CreateUser(string username, string mail)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("MailPost",mail);
+
+        WWW www = new WWW(CreateUserURL, form);
+    }
+}
